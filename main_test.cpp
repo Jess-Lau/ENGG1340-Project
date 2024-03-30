c81a88b7ba5adba07fac4ac0466eaccc37ed5640
RyanPangSY <rpsy6138@connect.hku.hk>
2024-03-30 22:54:19 +0800

    Test

--- main_test.cpp
+++ main_test.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 using namespace std;
 
+void clearScreen() {
+    cout << "\033[2J\033[1;1H";
+}
+
 void printIntro() {
-    cout << "\t\tWelcome to my game\n\n"
+    cout << "Welcome to my game\n\n"
             "You must collect all six items before fighting the boss.\n" << endl;
 
-    cout << "Press any key to continue...";
-    cin.ignore();
+    // Press any key to continue . . . 
+    system("pause");
+    clearScreen();
 }
+
+struct Rooms {
+    string name;
+    string east;
+    string south;
+    string west;
+    string north;
+    string item;
+};
 
 int main() {
-
+    printIntro();
+    cout << "Test" << endl;
 }
+
