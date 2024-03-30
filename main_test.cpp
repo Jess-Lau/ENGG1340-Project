45918c5473e1a192847a364d08b3f22143f6f130
RyanPangSY <79847180+RyanPangSY@users.noreply.github.com>
2024-03-30 21:24:09 +0800

    test

--- main_test.cpp
+++ main_test.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 // using namespace std;
 
-void prompt() {
+void printIntro() {
     std::cout << "\t\tWelcome to my game\n\n"
                  "You must collect all six items before fighting the boss.\n\n"
                  "Moves:\t'go {direction}' (travel north, south, east, or west)\n"
